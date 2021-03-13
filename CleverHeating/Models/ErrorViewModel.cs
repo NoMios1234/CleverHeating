fefbf03396c1fed832fd35c162cb8ff52894bdcf
Dmitry Buhai <NoMios>
2021-03-13 15:48:38 +0200

    create project

---
+++ CleverHeating/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleverHeating.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
